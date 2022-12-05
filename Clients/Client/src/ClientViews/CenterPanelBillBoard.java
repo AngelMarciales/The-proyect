47cd93cae67184130281aef6a23b8713bee83713
AngelMarciales <angel.marciales@uptc.edu.co>
2022-12-05 17:15:32 -0500

    Cliente finalizado

--- Clients/Client/src/ClientViews/CenterPanelBillBoard.java
+++ Clients/Client/src/ClientViews/CenterPanelBillBoard.java
@@ -19,18 +19,17 @@ public class CenterPanelBillBoard extends JPanel {
         this.setLayout(new CardLayout());
         cl = (CardLayout) this.getLayout();
         tarjetaVacia = new Tarjeta[15];
-        for (int i = 0; i < tarjetaVacia.length; i++) {
-            tarjetaVacia[i] = new Tarjeta();
-        }
     }
 
     public void addTarjet(String[] routes, String[] names){
-        for (int i = 1; i <= tarjetaVacia.length; i++) {
+        for (int i = 0; i <= tarjetaVacia.length; i++) {
             if(i < routes.length){
+                tarjetaVacia[i] = new Tarjeta();
                 tarjetaVacia[i].setIcon(routes[i]);
-                cl.addLayoutComponent(tarjetaVacia[i], names[i]);
+                add(tarjetaVacia[i], names[i]);
             }
         }
+
     }
 
     public void show(String a) {
